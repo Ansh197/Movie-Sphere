9bdb5e2ece3d5b6f11695c931d47039d82aac67d
Ansh197 <77569137+Ansh197@users.noreply.github.com>
2023-07-08 20:33:24 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ function App() {
         <Router>
           <Header />
             <Routes>
+                <Route path="/" element={<Home />}></Route>
                 <Route index element={<Home />}></Route>
                 <Route path="movie/:id" element={<Movie />}></Route>
                 <Route path="movies/:type" element={<MovieList />}></Route>
-                <Route path="/*" element={<Home />}></Route>
+                <Route path="/*" element={<h1>error page not found</h1>}></Route>
             </Routes>
         </Router>
     </div>
